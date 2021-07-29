6a74bbbd679bb0483b0545ec8fb165d9c21721ee
Frances <frances.calceta@gmail.com>
2021-07-29 15:49:52 +0800

    added lots of css

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   validates :user, presence: true
+  validates_length_of :content, :maximum => 500, :minimum => 1
+  validates_length_of :title, :maximum => 100, :minimum => 1
 
 end
